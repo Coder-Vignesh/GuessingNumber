2f67ff40d32c2eb90f5f9617ca43d28c53923158
Vignesh G <71144564+vignesh2311@users.noreply.github.com>
2021-06-08 16:40:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-var correctGuess = false;
-var randomNumber = Math.floor(Math.random() * 6 ) + 1;
-var guess = prompt("I Guess a number between 1 and 6. What is it?");
+//My JS File
+var correctGuess = false;//declare variable
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;//generate random number 1 to 6(also 1 and 6) 
+var guess = prompt("I Guess a number between 1 and 6. What is it?");//Getting number
 
 if (parseInt(guess) === randomNumber ) {
+  //it will return true and i store in correctGuess varible
   correctGuess = true;
 } 
 
 
 if ( correctGuess ) {
     document.write('');
-} else if (guess > randomNumber){ 
+} else if (guess > randomNumber){ //if enter greater than correct number it will show smaller than number
   var guessTooBig = prompt("Sorry, the correct number is smaller than " + guess + ". Please try again");
-} else if (guess < randomNumber){
+} else if (guess < randomNumber){//if enter smaller than correct number it will show smaller than number
   var guessTooSmall = prompt("Sorry, the correct number is bigger than " + guess + ". Please try again");
 } else {document.write(" ")
 }
@@ -27,7 +28,7 @@ if (parseInt(guessTooBig) === randomNumber ) {
 }
 
 if (correctGuess) {
-  document.write('<p>You Are Correct!!! the number is ' + randomNumber + '!</p>');
+  document.write('<p>You Are Correct!!! the number is ' + randomNumber + '!</p>');//print text
 } else {
     document.write('<p>Sorry You Are Wrong. The number was ' + randomNumber + '.</p>');
 }
